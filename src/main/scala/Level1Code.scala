f649b5ac3ae6151419ebcbe0414594c22d81fb00
mwmartin <mwm.cal@gmail.com>
2015-05-26 14:50:53 -0700

    Level1Code answers

--- src/main/scala/Level1Code.scala
+++ src/main/scala/Level1Code.scala
@@ -2,15 +2,15 @@
 object Level1Code {
 
   // Returns "Hello World!"
-  def helloWorld(): String = ???
+  def helloWorld(): String = "Hello World!"
 
   // return a stream that contains every integer starting at 'start'
-  def countFrom(start: Int): Stream[Int] = ???
+  def countFrom(start: Int): Stream[Int] = Stream.from(start)
 
   // return a stream that contains the square of every integer starting at 'start'
-  def squaresFrom(start: Int): Stream[Int] = ???
+  def squaresFrom(start: Int): Stream[Int] = countFrom(start).map(x => x * x)
 
   // return a stream that contains the product of factors from two streams
-  def productOfStreams(aFactors: Stream[Int], bFactors: Stream[Int]): Stream[Int] = ???
+  def productOfStreams(aFactors: Stream[Int], bFactors: Stream[Int]): Stream[Int] = aFactors.zip(bFactors).map(t => t._1 * t._2)
 
 }
