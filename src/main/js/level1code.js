782b57911902e5fde87cd0b352128e08c7a7c191
mwmartin <mwm.cal@gmail.com>
2015-05-27 12:59:04 -0700

    level1 answers

--- src/main/js/level1code.js
+++ src/main/js/level1code.js
@@ -1,32 +1,74 @@
 var code = {
   // Returns "Hello World!"
   helloWorld: function () {
-    throw new Error("Not Implemented");
+    return "Hello World!";
   },
   // Defines a singly-linked list implementation:
   linkedList: {
     head: null,
     tail: null,
     add: function (data) {
-      throw new Error("Not Implemented");
+      var makeNode = function (value) { return { data: value, next: null }; };
+      var newNode = makeNode(data);
+
+      if (!this.head) this.head = newNode;
+
+      if (this.tail) this.tail.next = newNode;
+
+      this.tail = newNode;
+      //throw new Error("Not Implemented");
     },
-    // Returns the smallest number in the list
+    run: function (func) {
+
+      var current = this.head;
+      var data = null;
+      while (current.next) {
+        data = func(current.data, data);
+        current = current.next;
+      }
+      return data;
+    },
+    // returns the smallest number in the list
     min: function () {
-      throw new Error("Not Implemented");
+      return this.run(function (current, data) {
+        if (data === null || current < data)
+          return current;
+        else
+          return data;
+      });
+      //throw new Error("Not Implemented");
     },
-    // Returns the largest number in the list
+    // returns the largest number in the list
     max: function () {
-      throw new Error("Not Implemented")
+      return this.run(function (current, data) {
+        if (data === null || current > data)
+          return current;
+        else
+          return data;
+      });
+      //throw new Error("Not Implemented")
     },
-    // Returns the length of the list
+    // returns the length of the list
     length: function () {
-      throw new Error("Not Implemented");
+      return this.run(function (_, data) {
+        if (data === null)
+          return 1;
+        else
+          return data + 1;
+      });
+      //throw new Error("Not Implemented");
     }
   },  
-  // Returns a linked list object that contains 
+  // return a linked list object that contains 
   // every integer starting at 'start' and ending at 'end'
   countFromTo: function (start, end) {
-    throw new Error("Not Implemented");
+
+    var list = Object.create(this.linkedList);
+    for (var i = start; i <= end; i++) {
+      list.add(i);
+    };
+    return list;
+    //throw new Error("Not Implemented");
   }
 };
 module.exports = code;
