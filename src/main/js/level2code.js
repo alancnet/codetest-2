4fb4a16a4d4fbad16fcc46a128614545415744f5
mwmartin <mwm.cal@gmail.com>
2015-05-27 18:23:40 -0700

    adding level2 answers

--- src/main/js/level2code.js
+++ src/main/js/level2code.js
@@ -1,15 +1,45 @@
 var code = {
 	// Returns an Array of n Fibonacci numbers
 	firstNFibonacciNumbers: function (n) {
-		throw new Error("Not Implemented");
+
+		var fibs = function (x) {
+			if (x === 0)
+				return 0;
+			if (x === 1)
+				return 1;
+			return fibs(x - 1) + fibs(x - 2);
+		};
+
+		var arr = new Array();
+		var cnt = 0;
+		while (cnt <= n) {
+			arr.push(fibs(cnt));
+			cnt++;
+		}
+
+		return arr;
+      //throw new Error("Not Implemented");
 	},
 	// Determines whether n is a Fibonacci number
 	isFibonacciNumber: function (n) {
-		throw new Error("Not Implemented");
+		var fibs = this.firstNFibonacciNumbers(n + 1);
+		for (var i = 0; i <= fibs.length; i++) {
+			if (fibs[i] === n)
+				return true;
+		}
+		return false;
+      //throw new Error("Not Implemented");
 	},
 	// Sums all Fibonacci numbers between lower and upper inclusive
 	sumSomeFibonacciNumbers: function (lower, upper) {
-		throw new Error("Not Implemented");
+		var fibs = this.firstNFibonacciNumbers(upper);
+		var value = 0;
+		for (var i = 0; i <= fibs.length; i++) {			
+			if (fibs[i] >= lower && fibs[i] <= upper)
+				value += fibs[i];
+		}
+		return value;
+      //throw new Error("Not Implemented");
 	}
 };
 module.exports = code;
