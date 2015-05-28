c02bb1f1e8e63aa6fedc15f0d46e3c46c444ef8e
mwmartin <mwm.cal@gmail.com>
2015-05-27 21:03:03 -0700

    adding answers for new level1 tests

--- src/main/js/level1code.js
+++ src/main/js/level1code.js
@@ -5,54 +5,54 @@ var code = {
     },
     // Returns the sum of two numbers
     sum: function (a, b) {
-        throw new Error("Not Implemented");
+        return a + b;
     },
     // Returns the product of two numbers
     product: function (a, b) {
-        throw new Error("Not Implemented");
+        return a * b;
     },
     // Returns the square of a number
     square: function (a) {
-        throw new Error("Not Implemented");
+        return a * a;
     },
     // Returns the cube of a number
     cube: function (a) {
-        throw new Error("Not Implemented");
+        return a * a * a;
     },
     // Returns the square root of a perfect square
     sqrt: function (a) {
-        throw new Error("Not Implemented");
+        return Math.sqrt(a);
     },
     // Returns the Int quotient of a fraction
     quotient: function (numerator, denominator) {
-        throw new Error("Not Implemented");
+        return Math.round(numerator / denominator);
     },
     // Returns the Int remainder of a fraction
     remainder: function (numerator, denominator) {
-        throw new Error("Not Implemented");
+        return numerator % denominator;
     },
     // Returns an array containing the square of each number
     // in the source array in the same order
     squareAll: function (arr) {
-        throw new Error("Not Implemented");
+        return arr.map(function (x) { return x * x; });
     },
     // Returns an array containing the cube of each number
     // in the source array in the same order
     cubeAll: function (arr) {
-        throw new Error("Not Implemented");
+        return arr.map(function (x) { return x * x * x; });
     },
     // Returns an array containing the product of an 'a' in array 'arrA' with
     // its respective 'b' in array 'arrB'
     productAll: function (arrA, arrB) {
-        throw new Error("Not Implemented");
+        return arrA.map(function (a, i) { return a * arrB[i]; });
     },
     // Sums all of the elements in the array and returns the result
     sumAll: function (arr) {
-        throw new Error("Not Implemented");
+        return arr.reduce(function (prev, acc) { return prev + acc; });
     },
     // Reverses the array
     reverse: function (arr) {
-        throw new Error("Not Implemented");
+        return arr.reverse();
     },
     // Defines a singly-linked list implementation:
     linkedList: {
